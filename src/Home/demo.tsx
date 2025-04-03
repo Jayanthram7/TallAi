39b3a2f0f5f09ff78dcfdd2023190e126359ea48
Jayanthram7 <121469111+Jayanthram7@users.noreply.github.com>
2025-04-03 14:34:21 +0530

    Update demo.tsx

--- src/Home/demo.tsx
+++ src/Home/demo.tsx
@@ -32,7 +32,7 @@ export function HeroScrollDemo() {
           }
         >
           <img
-            src="https://ui.aceternity.com/_next/image?url=%2Flinear.webp&w=3840&q=75"
+            src="https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="hero"
             height={480}
             width={800}
