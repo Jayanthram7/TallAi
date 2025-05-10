6da12c27da98e204d0607156c7216d44855263c5
Jayanthram7 <jayanthramnithin@gmail.com>
2025-05-10 20:12:39 +0530

    employee navbar change

--- src/Home/NavbarEmp.js
+++ src/Home/NavbarEmp.js
@@ -60,7 +60,7 @@ export default function Navbar() {
             >
               <span className="absolute -inset-1.5" />
               <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="size-6" />
+             
             </button>
 
             {/* Profile dropdown */}
@@ -80,22 +80,7 @@ export default function Navbar() {
                 transition
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none"
               >
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
+                
                 <MenuItem>
   <Link
     to="/"
